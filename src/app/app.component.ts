2a85c0dee314c6dd359d3614e51d0c9a89544f19
abderrahimissaoui <aissaoui_abderrahim@hotmail.fr>
2023-02-08 13:47:16 +0100

    introduce eth get account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { MetaMaskInpageProvider } from '@metamask/providers';
 
+declare global {
+  interface Window {
+    ethereum: MetaMaskInpageProvider;
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'metamask';
+
+  enableEthereum() {
+    const ethereum = window.ethereum as MetaMaskInpageProvider;
+    // TODO this is a promise
+    ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async getAccount() {
+    console.log('getAccount');
+    const showAccount = document.querySelector('.showAccount');
+    const ethereum = window.ethereum as MetaMaskInpageProvider;
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' }) as any;
+    const account = accounts[0];
+    console.log(accounts);
+    showAccount!.innerHTML = account;
+  }
+
+  async waitForAccount(): Promise<void> {
+    console.log('waitForAccount');
+    const ethereum = window.ethereum as MetaMaskInpageProvider;
+    const showAccount = document.querySelector('.showAccount');
+        try {
+          const accounts = await ethereum.request({ method: 'eth_requestAccounts' }) as any;
+            console.log(accounts);
+            if (accounts!.length > 0) {
+              showAccount!.innerHTML = accounts[0];
+            }
+        } catch (e) {
+          console.log(e) 
+        }
+  }
 }
