00f4f6722a994a0cbf7b1eb088cfa38bd374d298
abderrahimissaoui <aissaoui_abderrahim@hotmail.fr>
2023-02-08 17:03:21 +0100

    Add metamask send transaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,4 +44,49 @@ export class AppComponent {
           console.log(e) 
         }
   }
+
+  async selectedAddress(): Promise<void> {
+    console.log('selectedAddress');
+    const ethereum = window.ethereum as MetaMaskInpageProvider;
+    const selectedAddress = document.querySelector('.selectedAddress');
+        try {
+          const accounts = ethereum.selectedAddress as any;
+            console.log(accounts);
+            selectedAddress!.innerHTML = accounts;
+        } catch (e) {
+          console.log(e) 
+        }
+  }
+
+  async sendTransaction(toAddress: string, value: string){
+
+
+
+    const ethereum = window.ethereum as MetaMaskInpageProvider;
+    console.log(toAddress);
+    
+    if(toAddress.toLowerCase() == ethereum.selectedAddress?.toLowerCase()) {
+      alert('you are sending to the same account youuu idiot');
+    } else {
+      const transactionParameters = {
+        nonce: '0x00', // ignored by MetaMask
+        gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
+        gas: '0x5208', // customizable by user during MetaMask confirmation.
+        to: toAddress, // Required except during contract publications.
+        from: ethereum.selectedAddress, // must match user's active address.
+        value: value, // Only required to send ether to the recipient from the initiating external account.
+        data:
+          '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
+        chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
+      };
+      
+      // txHash is a hex string
+      // As with any RPC call, it may throw an error
+      const txHash = await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [transactionParameters],
+      });
+    }
+
+  }
 }
